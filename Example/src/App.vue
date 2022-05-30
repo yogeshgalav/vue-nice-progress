77565c239294fc402442c714051a9943f5db9ba1
monti09 <gautamgjhaj@gmail.com>
2022-05-31 01:12:04 +0530

    added example-progress

---
+++ Example/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <VueNiceProgress :value="50"/>
+  </div>
+</template>
+
+
+<script>
+import  VueNiceProgress  from '../../src/VueNiceProgress.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VueNiceProgress,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
